0cbb19e61b2514bc84044908885df5ca199bfdcf
Виталий <fomenkov@indins.ru>
2022-04-17 04:40:51 +0300

    add jquery

---
+++ assets/auth.js
@@ -0,0 +1,33 @@
+$(document).ready(auth);
+
+const ticket = 'talk_token'
+
+function auth()
+{
+    $('#auth_submit_button').click(function (event) {
+        let username = $('#username').val();
+        let password = $('#password').val();
+        $.ajax({
+            type: "POST",
+            accept: "application/json",
+            contentType: "application/json",
+            url: '/api/auth/sign-in',
+            data: JSON.stringify({
+                username: username,
+                password: password
+            }),
+            dataType: "json",
+            success: saveToken
+        })
+    })
+}
+
+function saveToken(data)
+{
+    window.localStorage.setItem(ticket, data.token)
+}
+
+function token()
+{
+    window.localStorage.getItem(ticket)
+}
